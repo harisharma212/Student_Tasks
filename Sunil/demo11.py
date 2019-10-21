d3c169c6cdec7598e88639885d397c68a88530df
murali1898 <seginenibalamurali@gmail.com>
2019-10-21 16:48:56 +0530

    Demo File

---
+++ Sunil/demo11.py
@@ -0,0 +1,75 @@
+name = 'python is a world famous programing language is great'
+print(name.count(' '))
+
+print(len(name))
+
+print(round(10.90))
+print(round(19249.985759,4))
+
+import math
+print(math.floor(10.5))
+
+'''name=input("enter your age")
+print(name)'''
+
+xyz = "murali royal"
+#print(dir(xyz))
+print(xyz.capitalize())
+print(xyz.upper())
+
+
+abc = "MURALI royal"
+print(abc.lower())
+
+ycp = "raja king is a good boy "
+print(ycp.index("i",7,))
+
+tdp = "chandra,babu,nayudu,is,a,very,bad,boy and he is a jaffa"
+print(tdp.isalnum())
+print(tdp.split())
+print(tdp.split(","))
+print(tdp.split("a",2))
+print(tdp.strip())
+
+
+bjp = "NARENDRA MODI IS a good boy"
+print(bjp.strip(" "))
+print(bjp.swapcase())
+
+jsp = "pawan kalyan is a star"
+print(jsp.replace("a","@"))
+print(jsp.zfill(40))
+
+s = "dear {},your ticket has been booked on {} from {} to {}"
+print(s.format("murali","20-09-2019","hyderabad","bangloor"))
+
+party = "pawan"
+print(party.startswith("p"))
+
+
+xxx = "murali is a good boy"
+print(xxx.split())
+print("".join(xxx))
+
+rrr = "murali"
+print(rrr[0:1:1])
+print(rrr[0:7:1])
+print(rrr[-1:-7:-1])
+print(rrr[-1:-7:-2])
+print(rrr[0:7:2])
+print(rrr[-1:-3:-1])
+print(rrr[1:-3:1])
+print(rrr[3:-7:-2])
+print(rrr[-2:1:1])
+print(rrr[-2:6:2])
+print(rrr[-1:-7])
+print(rrr[0:7])
+print(rrr[-1:6])
+print(rrr[-2:1])
+print(rrr[-2:6])
+print(rrr[6:-1])
+print(rrr[3:6])
+print(rrr[3:-7:-1])
+print(rrr[-2:6:2])
+print(rrr[0:6:-2])
+print(rrr[-1:6])
